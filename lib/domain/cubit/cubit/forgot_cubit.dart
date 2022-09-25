6b2da44ef040cc40fba62221bc4146c314928ab6
Noah <Ingenierostiven@outlook.es>
2022-09-24 20:48:32 -0500

    autentication

--- lib/domain/cubit/cubit/forgot_cubit.dart
+++ lib/domain/cubit/cubit/forgot_cubit.dart
@@ -1,8 +1,32 @@
-import 'package:bloc/bloc.dart';
+import 'package:ambient/data/autentication_services.dart';
 import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'forgot_state.dart';
 
 class ForgotCubit extends Cubit<ForgotState> {
-  ForgotCubit() : super(ForgotInitial());
+  final AutenticationServices _autenticationServices;
+  ForgotCubit(AutenticationServices autenticationServices)
+      : _autenticationServices = autenticationServices,
+        super(ForgotInitial());
+
+  Future sendEmail(String email) async {
+    try {
+      emit(ForgotLoading());
+      await _autenticationServices.forget(email: email);
+      emit(ForgotLoaded());
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case "user-not-found":
+          emit(const ForgotError("Usuario no encontrado"));
+          break;
+        case "invalid-email":
+          emit(const ForgotError("Correo invalido"));
+          break;
+        default:
+          emit(const ForgotError("Error"));
+      }
+    }
+  }
 }
---
+++ lib/domain/services/navitation_manage.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/cupertino.dart';
+
+class NavigatorManager {
+  static void pushFadeTransition(
+      {required BuildContext context, required Widget page}) {
+    Navigator.push(
+      context,
+      PageRouteBuilder(
+        pageBuilder: (context, animation, anotherAnimation) {
+          return page;
+        },
+        transitionDuration: const Duration(milliseconds: 1000),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          animation =
+              CurvedAnimation(parent: animation, curve: Curves.bounceIn);
+          return FadeTransition(
+            opacity: animation,
+            child: child,
+          );
+        },
+      ),
+    );
+  }
+
+  static void pushAndRemoveFadeTransition(
+      {required BuildContext context, required Widget page}) {
+    Navigator.pushAndRemoveUntil(
+        context,
+        PageRouteBuilder(
+          pageBuilder: (context, animation, anotherAnimation) {
+            return page;
+          },
+          transitionDuration: const Duration(milliseconds: 1000),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            animation =
+                CurvedAnimation(parent: animation, curve: Curves.bounceIn);
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+        ),
+        ((route) => false));
+  }
+}
